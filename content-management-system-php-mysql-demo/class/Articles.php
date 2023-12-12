a94b25e473420529dcb0c073e855a44558a01b34
DoHoang <hoangdo18022003@gmail.com>
2023-12-13 01:08:56 +0700

    Sua main

--- content-management-system-php-mysql-demo/class/Articles.php
+++ content-management-system-php-mysql-demo/class/Articles.php
@@ -1,21 +1,28 @@
 <?php
-	public function getArticles(){
+	class Article{
+		private $id;
+		private $postTable = 'cms_posts';
+		private $categoryTable = 'cms_category';
+		private $userTable = 'cms_user';
+		private $conn;
+		public function getArticles(){
 
-		$query = '';
-		if($this->id) {		
-			$query = " AND p.id ='".$this->id."'";
+			$query = '';
+			if($this->id) {		
+				$query = " AND p.id ='".$this->id."'";
+			}
+			$sqlQuery = "
+				SELECT p.id, p.title, p.message, p.category_id, u.first_name, 
+			u.last_name, p.status, p.created, p.updated, c.name as category
+				FROM ".$this->postTable." p
+				LEFT JOIN ".$this->categoryTable." c ON c.id = p.category_id
+				LEFT JOIN ".$this->userTable." u ON u.id = p.userid
+				WHERE p.status ='published' $query ORDER BY p.id DESC";
+				
+			$stmt = $this->conn->prepare($sqlQuery);		
+			$stmt->execute();
+			$result = $stmt->get_result();	
+			return $result;	
 		}
-		$sqlQuery = "
-			SELECT p.id, p.title, p.message, p.category_id, u.first_name, 
-		u.last_name, p.status, p.created, p.updated, c.name as category
-			FROM ".$this->postTable." p
-			LEFT JOIN ".$this->categoryTable." c ON c.id = p.category_id
-			LEFT JOIN ".$this->userTable." u ON u.id = p.userid
-			WHERE p.status ='published' $query ORDER BY p.id DESC";
-			
-		$stmt = $this->conn->prepare($sqlQuery);		
-		$stmt->execute();
-		$result = $stmt->get_result();	
-		return $result;	
 	}
 ?>
